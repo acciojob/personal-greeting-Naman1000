2e604b3ba179478de0395feb94887ed9c16c96cf
Naman1000 <91007344+Naman1000@users.noreply.github.com>
2024-12-03 17:55:13 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+const Greeting = ({ name }) => {
+  return (
+    <div>
+      <h2>Hello {name}</h2>
+    </div>
+  );
+};
+
+export default Greeting;
+
+
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
