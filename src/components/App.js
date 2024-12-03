84e71728c8ece106c789c9d5b3b3da3d8c1f4aa4
Naman1000 <91007344+Naman1000@users.noreply.github.com>
2024-12-03 19:04:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
 
 import React from "react";
 import './../styles/App.css';
-import { Greeting } from './index';
+
+const Greeting = (props) => {
+  return <h1>Hello, {props.name}!</h1>
+}
+
+
+
 
 const App = () => {
   return (
